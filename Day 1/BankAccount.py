4d30107b4b1a49e37503eee1503dfbed5ab3066e
anchal0404 <anchalrathore3320@gmail.com>
2021-12-21 12:14:56 +0530

    new

--- Day 1/BankAccount.py
+++ Day 1/BankAccount.py
@@ -1,12 +1,10 @@
 print('Welcome to Dena Bank')
 balance=10000
 exist=input('Do You have any account YES and NO?')
-if exist == "YES":
+if (exist == "y"):
     X = int(input('Enter you Account Number'))
 else:
     print("Please Create new account")
-
-
 print('Do you want to Credit or Deposit?')
 u=input('enter C for credit and D for deposit \n')
 if (u =='C'):
@@ -21,7 +19,7 @@ elif (u=='D'):
     amount=balance-f
     print(amount)
 else:
-    print("thankyou")
+    print("SOMETHING WENT WRONG")
    
 
    
