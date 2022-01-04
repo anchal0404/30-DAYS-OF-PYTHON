ba45dde3f4e0d71e12c6c5672ac4c3a289f62e5f
anchal0404 <anchalrathore3320@gmail.com>
2022-01-04 22:18:09 +0530

    FlaskApi.py

--- Day11/Flaskapi.py
+++ Day11/Flaskapi.py
@@ -7,20 +7,20 @@ def horo(n):
     import re
     import pandas as pd    
 
-page = urllib.request.urlopen("https://www.horoscope.com/us/index.aspx")
-soup = bs(page)
+    page = urllib.request.urlopen("https://www.horoscope.com/us/index.aspx")
+    soup = bs(page)
 
-names = soup.body.findAll('a')
-function_names= re.findall('id="src-hp-.\w+',str(names))
-function_names=[item[11:] for item in function_names]
+    names = soup.body.findAll('a')
+    function_names= re.findall('id="src-hp-.\w+',str(names))
+    function_names=[item[11:] for item in function_names]
 
-description=soup.body.find_all('p')
-function_usage=[]
+    description=soup.body.find_all('p')
+    function_usage=[]
 
-for item in description:
-    item=item.text
-    item=item.replace('/n','')
-    function_usage.append(item)
+    for item in description:
+     item=item.text
+     item=item.replace('/n','')
+     function_usage.append(item)
     s = 0
 
     for i in range(0,12):
@@ -32,7 +32,7 @@ for item in description:
     data = {'Zodiac': function_names[s], 'Date': date[s]}
     
     
-    print jsonify(data)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
